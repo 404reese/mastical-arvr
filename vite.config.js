4536c79f006bf9acfaafc19b32b0818cac1aa6d0
Riddhesh <riddhesh.chaudhary@gmail.com>
2025-01-11 11:53:16 +0530

    v3-config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 export default {
-    root: './src',
+    root: '.',                 // Pointing to the project root where index.html is located
     server: {
-        open: true,
+      open: true,              // Automatically opens the browser when the server starts
+      port: 5173,              // Explicitly set the port to 5173
     },
     build: {
-        outDir: '../dist',
+      outDir: './dist',        // Output the build to the 'dist' folder in the root
     },
-    publicDir: '../src/assets', // Point to the directory containing static files
-};
+    publicDir: './assets',     // Static assets (like images) are in the 'assets' folder at the root level
+  };
+  
